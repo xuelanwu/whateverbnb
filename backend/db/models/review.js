85624f0188b08229308dd8aa251d5157160d22a8
XueLan Wu <xuelanwu823@gmail.com>
2022-10-24 19:39:16 -0400

    generate models and migrations

---
+++ backend/db/models/review.js
@@ -0,0 +1,29 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Review extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Review.init(
+    {
+      spotId: DataTypes.INTEGER,
+      userId: DataTypes.INTEGER,
+      review: DataTypes.STRING,
+      stars: DataTypes.INTEGER,
+      createdAt: { type: DataTypes.DATE, allowNull: false },
+      updatedAt: { type: DataTypes.DATE, allowNull: false },
+    },
+    {
+      sequelize,
+      modelName: "Review",
+    }
+  );
+  return Review;
+};
---
+++ backend/db/models/spot.js
@@ -0,0 +1,35 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Spot extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Spot.init(
+    {
+      ownerId: { type: DataTypes.INTEGER, allowNull: false },
+      address: { type: DataTypes.STRING, allowNull: false },
+      city: { type: DataTypes.STRING, allowNull: false },
+      state: { type: DataTypes.STRING, allowNull: false },
+      country: { type: DataTypes.STRING, allowNull: false },
+      lat: DataTypes.DECIMAL,
+      lng: DataTypes.DECIMAL,
+      name: DataTypes.STRING,
+      description: DataTypes.STRING,
+      price: DataTypes.DECIMAL,
+      createdAt: { type: DataTypes.DATE, allowNull: false },
+      updatedAt: { type: DataTypes.DATE, allowNull: false },
+    },
+    {
+      sequelize,
+      modelName: "Spot",
+    }
+  );
+  return Spot;
+};
