f8b33cebc4b243b751d52fd83e9ef13086c87998
XueLan Wu <xuelanwu823@gmail.com>
2022-11-11 20:29:12 -0500

    added Navigation component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import { restoreUser } from "./store/session";
 
 import LoginFormPage from "./components/LoginFormPage";
 import SignupFormPage from "./components/SignupFormPage";
+import Navigation from "./components/Navigation";
 
 function App() {
   const dispatch = useDispatch();
@@ -16,16 +17,19 @@ function App() {
   }, [dispatch]);
 
   return (
-    isLoaded && (
-      <Switch>
-        <Route path="/login">
-          <LoginFormPage />
-        </Route>
-        <Route path="/signup">
-          <SignupFormPage />
-        </Route>
-      </Switch>
-    )
+    <div>
+      <Navigation isLoaded={isLoaded} />
+      {isLoaded && (
+        <Switch>
+          <Route path="/login">
+            <LoginFormPage />
+          </Route>
+          <Route path="/signup">
+            <SignupFormPage />
+          </Route>
+        </Switch>
+      )}
+    </div>
   );
 }
 
