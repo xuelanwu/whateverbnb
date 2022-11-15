7252df43f183f4cfc89409b664d863e8a3f123b8
XueLan Wu <xuelanwu823@gmail.com>
2022-11-15 10:13:28 -0500

    spot detail page added

--- frontend/src/components/SpotDetailPage/index.js
+++ frontend/src/components/SpotDetailPage/index.js
@@ -1,4 +1,20 @@
+import { useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { fetchSpotDetail } from "../../store/spot";
+
 const SpotDetailPage = () => {
+  const { spotId } = useParams();
+  console.log(spotId);
+  const dispatch = useDispatch();
+  const spot = useSelector((state) => state.spot);
+
+  useEffect(() => {
+    dispatch(fetchSpotDetail(spotId));
+  }, [dispatch]);
+
+  console.log(spot);
+
   return <div>'spot detail page'</div>;
 };
 
