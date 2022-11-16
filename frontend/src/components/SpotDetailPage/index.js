d62ddeae8caddd535bb7d2bfdc1392dcab028882
XueLan Wu <xuelanwu823@gmail.com>
2022-11-16 01:33:43 -0500

    add css file

--- frontend/src/components/SpotDetailPage/index.js
+++ frontend/src/components/SpotDetailPage/index.js
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { fetchSpotDetail } from "../../store/spot";
+import "./index.css";
 
 const SpotDetailPage = () => {
   const { spotId } = useParams();
@@ -10,20 +11,26 @@ const SpotDetailPage = () => {
 
   useEffect(() => {
     dispatch(fetchSpotDetail(spotId));
-  }, [dispatch]);
+  }, [dispatch, spotId]);
 
   if (!spot) return null;
 
   console.log(spot.SpotImage);
   return (
-    <div>
+    <div className="spot-detail-container">
       <div>{spot.name}</div>
       <div>{`${spot.city}, ${spot.country}`}</div>
       <div>{`stars ${spot.avgStarRating}`}</div>
       <div>{`${spot.numReviews} reviews`}</div>
-      <div>
+      <div className="spot-image-container">
         {spot.SpotImage &&
-          spot.SpotImage.map((img) => <img src={img.url} alt={spot.name} />)}
+          spot.SpotImage.map((img) => (
+            <img
+              src={img.url}
+              alt={spot.name}
+              style={{ width: "200px", height: "200px" }}
+            />
+          ))}
       </div>
     </div>
   );
