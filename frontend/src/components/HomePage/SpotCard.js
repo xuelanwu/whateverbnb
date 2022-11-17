35224454dec6400ee7c62e53005cb9f6499ff094
XueLan Wu <xuelanwu823@gmail.com>
2022-11-17 00:35:33 -0500

    css on home page spot card

--- frontend/src/components/HomePage/SpotCard.js
+++ frontend/src/components/HomePage/SpotCard.js
@@ -2,14 +2,16 @@ import { NavLink } from "react-router-dom";
 
 const SpotCard = ({ spot }) => {
   return (
-    <NavLink to={`/spots/${spot.id}`}>
+    <NavLink to={`/spots/${spot.id}`} className="navlink-spot-card">
       <div className="spot-card">
         <div className="spot-card-image-box">
           {spot.previewImage !== "Let's add some photos!" && (
             <img src={spot.previewImage} alt={spot.name} />
           )}
         </div>
-        <div className="spot-card-city-box">{spot.city}</div>
+        <div className="spot-card-city-box">
+          <p>{`${spot.city},${spot.state}`}</p>
+        </div>
         <div className="spot-card-price-box">{`$${spot.price} night`}</div>
       </div>
     </NavLink>
